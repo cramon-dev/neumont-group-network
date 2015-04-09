efbe4f4287b447c423be3c03eb5a70738405d6d4
reticen <christianr465@gmail.com>
2015-04-08 20:59:40 -0600

    registration.js can now handle POST requests. registration-controller.js can now connect to a MySQL database and perform basic queries. Added home.jade to be used later, what a user will see when they log in or register for the first time. Fixed a button in register.jade to actually perform a POST request.

--- resources/js/registration-controller.js
+++ resources/js/registration-controller.js
@@ -1,4 +1,42 @@
 var http = require('http');
+var mysql = require('mysql');
 //Will I need to use crypto here?
 //var crypto = require('crypto');
 
+//var DB = db.DB;
+//var BaseRow = db.Row;
+//var BaseTable = db.Table;
+
+//DAILY REMINDER TO SANITIZE ANY USER INPUT BEFORE CREATING A QUERY
+
+var createConnectionToDB = function() {
+    console.log("Creating connection to database..");
+    
+    var db_conn = new mysql.createConnection({
+        host: 'localhost',
+        user: 'ngn-user',
+        password: 'zJ7m9cdujSGrSvAq',
+        database: 'ngn-db'
+    });
+    
+    console.log("Calling connect function");
+    db_conn.connect();
+    
+    console.log("Returning connection: " + db_conn);
+    return(db_conn);
+}
+
+var executeBasicQuery = function(db_conn) {
+    console.log("Executing basic query");
+    db_conn.query('SELECT * from users', function(err, rows, fields) {
+        if (err) {
+            throw err;
+        }
+
+        console.log('Results: ', rows);
+    });
+    console.log("Basic query executed");
+}
+
+module.exports.createConnectionToDB = createConnectionToDB;
+module.exports.executeBasicQuery = executeBasicQuery;
--- routes/registration.js
+++ routes/registration.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var registrationCtrl = require('../resources/js/registration-controller.js');
 
 /* GET register page. */
 router.get('/', function(req, res, next) {
     res.render('register', { title: 'Register' });
+});
+
+//Upon successful user account registration, redirect to logged in home page?
+router.post('/', function(req, res, next) {
+    var db_conn = registrationCtrl.createConnectionToDB();
+    registrationCtrl.executeBasicQuery(db_conn);
+    //res.render('home', { username: their_username } ); //render home page with their username to show they're logged in?
 });
 
 module.exports = router;
